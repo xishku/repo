7e855ae8f5056c447206025d8741dfe24711570c
paul.ge <gejianquan@139.com>
2017-07-15 06:20:12 +0800

    java helloo

---
+++ helloworld/src/HelloWorldJava.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ms-sh on 2017/7/15.
+ */
+public class HelloWorldJava {
+
+        public static void main(String[] args){
+            System.out.println("Hello World!");
+        }
+
+
+}
