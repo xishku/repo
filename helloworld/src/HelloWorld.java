ab53f4ebd70e9f95232256ed95bb2bdd5f6cc37a
paul.ge <gejianquan@139.com>
2018-12-21 21:16:46 +0800

    add

---
+++ helloworld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ms-sh on 2018/12/21.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        //tst
+        System.out.println("Hello World!");
+    }
+}
