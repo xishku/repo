bb92b914c3f7f51b69a66f6e1a961a83e1541ed0
paul.ge <gejianquan@139.com>
2018-12-29 21:15:09 +0800

    BigNum

--- helloworld/src/BigNumber.java
+++ helloworld/src/BigNumber.java
@@ -28,7 +28,6 @@ public class BigNumber {
             rstNum[i] = 0;
         }
 
-
         for(int i = 0; i <  revLeftNumStr.length(); ++i)
         {
             char leftChar = revLeftNumStr.charAt(i);
@@ -46,9 +45,7 @@ public class BigNumber {
                         System.out.println("error character in string ".concat(m_BigNumStr));
                         return otherNum;
                     }
-
                 }
-
             }
             else
             {
@@ -59,21 +56,25 @@ public class BigNumber {
 
         for ( int i = 0; i < len - 1 ; ++i )
         {
-            System.out.println("now cal No.".concat(Integer.toString(i)));
+            //System.out.println("now cal No.".concat(Integer.toString(i)));
             rstNum[i + 1] += rstNum[i] / 10;
             rstNum[i] = rstNum[i] % 10;
         }
 
-
         String rstStr = "";
         for ( int i = len - 1; i >= 0; --i )
         {
-            System.out.println(rstNum[i]);
+            //System.out.println(rstNum[i]);
             rstStr = rstStr.concat(Integer.toString(rstNum[i]));
         }
 
         while (!rstStr.isEmpty())
         {
+            if (rstStr.equals("0"))
+            {
+                break;
+            }
+
             if (rstStr.startsWith("0"))
             {
                 rstStr = rstStr.substring(1, rstStr.length());
@@ -84,14 +85,8 @@ public class BigNumber {
             }
         }
 
-        if (rstStr.isEmpty())
-        {
-            rstStr = "0";
-        }
-
         System.out.println(rstStr);
         return new BigNumber(rstStr);
-
     }
 
 }
