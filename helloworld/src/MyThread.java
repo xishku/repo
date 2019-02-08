2537a5523cd668ff747fcc3afe690f7f94a56fee
paul.ge <gejianquan@139.com>
2019-02-08 21:21:59 +0800

    thread demo

--- helloworld/src/MyThread.java
+++ helloworld/src/MyThread.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by ms-sh on 2019/2/7.
@@ -7,18 +8,27 @@ public class MyThread extends Thread {
 
     public void run()
     {
-        sleep(1000 * ((int)(1+Math.random()) mod 10)));
-        System.out.println(this.getId());
-        System.out.println(this.getName());
-        System.out.println(this.getPriority());
-        System.out.println(this.getThreadGroup());
+        for (int i = 0; i < 5 ; i++) {
+            try {
+                Thread.sleep(1000 * (new Random()).nextInt( 20));
+                System.out.println(this.getName());
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //System.out.println(this.getId());
+        //System.out.println(this.getName());
+        //System.out.println(this.getPriority());
+        //System.out.println((new Random()).nextInt( 20));
     }
 
     public static void main(String [] args)
     {
         for (int i = 0; i < 10; i++) {
             Thread t = new MyThread();
-            t.run();
+            t.start();
         }
     }
 }
