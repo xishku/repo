dc8749a94524d24b244fe357326a2d2e4fba4367
FreeSpace <gejianquan@139.com>
2019-08-25 11:41:55 +0800

    Create HwTest.java

---
+++ helloworld/src/HwTest.java
@@ -0,0 +1,28 @@
+import static java.lang.String.valueOf;
+
+public class HwTest {
+
+    public static long testLoop(long len)
+    {
+        long startTime = System.nanoTime();
+        for (long i = 0; i < len; i++) {
+
+        }
+        long estimatedTime = System.nanoTime() - startTime;
+        //System.out.println(estimatedTime);
+
+        return estimatedTime;
+    }
+
+    public static void main(String args[]) {
+        //tst
+        long num = 100;
+        System.out.println("Hello World!");
+        for (int i = 0; i < 7; i++) {
+            num *= 10;
+            System.out.println(valueOf(num).concat("         ").concat(valueOf(testLoop(num))));
+        }
+    }
+
+
+}
